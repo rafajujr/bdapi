0c37ee93ac907ff68fb66cb10bab343bb506a300
rafael <rafaeljulianabe@gmail.com>
2024-03-02 18:54:43 -0300

    Adicionado cors

--- src/app.js
+++ src/app.js
@@ -5,11 +5,29 @@ dotenv.config();
 import './database';
 
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
 import homeRoutes from './routes/HomeRoutes';
 import userRoutes from './routes/UserRoutes';
 import tokenRoutes from './routes/TokenRoutes';
 import alunoRoutes from './routes/AlunoRoutes';
 import fotoRoutes from './routes/FotoRoutes';
+
+const whiterList = [
+  'nomeDoDomino',
+  'http//localhost:300',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiterList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -19,6 +37,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
